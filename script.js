9eb4ac0a55a906a45da272380914fd88dfffe5d3
kevinn-gonzales <kevinngstem@gmail.com>
2026-06-04 01:56:09 -0700

    added loader animation

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const textInput = document.getElementById('text-input');
 const nameInput = document.getElementById('name-input');
 const posts = document.getElementById('posts');
 const postNames = document.getElementById('names');
+const loading = document.getElementById('loader');
 
 //variable to store the url of the backend server where we will send requests to get and create posts
 const apiUrl = 'https://bulletin-board-8put.onrender.com/posts';
 
 
+//function responsible for creating a new div element for each post and appending it to the posts element
 function displayPosts(post) {
     const postElement = document.createElement('div');
+
+    //set the innerHTML of the post element to include the message and name of the post
     postElement.innerHTML = `
         <p><strong>Message:</strong> ${post.message}</p>
         <p><strong>Written by:</strong> ${post.name}</p>
@@ -22,21 +26,26 @@ function displayPosts(post) {
 
 
 
-
+//function responsible for fetching posts from the backend server and displaying them on the page
 async function fetchPosts() {
     try {
+        loading.style.display = 'block'; //show the loading animation while fetching posts
+
         const response = await fetch(apiUrl);
         const savedPosts = await response.json();
-
-        console.log(savedPosts);
         
         posts.innerHTML = '';
 
         savedPosts.forEach(function (post) {
             displayPosts(post);
         });
+
+        console.log('Posts successfully loaded');
+
     } catch (error) {
         console.error('Error Loading posts:', error);
+    } finally {
+        loading.style.display = 'none'; //hide the loading animation after posts are loaded
     }
 }
 
@@ -81,6 +90,8 @@ form.addEventListener('submit', async function (event) {
 
         displayPosts(savedPost);
 
+        console.log('Post successfully created');
+
         textInput.value = '';
         nameInput.value = '';
 
@@ -90,4 +101,6 @@ form.addEventListener('submit', async function (event) {
 });
 
 fetchPosts();
+
+
 
